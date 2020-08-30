52661ce340a22e37afdf73c03c36df778da384b9
luisfff29 <luisfff29@gmail.com>
2020-08-30 01:59:02 -0500

    GET and POST requests for /users

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client } = require("pg");
 const express = require("express");
+const PORT = 3000;
 
 // create an express application
 const app = express();
@@ -10,8 +11,32 @@ const client = new Client({
 });
 
 // route handlers go here
+app.get("/users", (req, res) => {
+  client.query("SELECT * FROM users", (err, result) => {
+    res.send(result.rows);
+  });
+});
+
+app.post("/users", (req, res) => {
+  const text = "INSERT INTO users (username, bio) VALUES ($1, $2) RETURNING *";
+  const values = [
+    "kenzie",
+    "Kenzie Academy is a user experience design and coding school in Indianapolis, Indiana. Our 6-month to 2-year program with 1-year paid apprenticeship is a new alternative to traditional colleges and short-term coding bootcamps.",
+  ];
+  client.query(text, values, (err, result) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      console.log(result.rows[0]);
+      res.json({
+        message: "User was successfully inserted into 'users' table.",
+      });
+    }
+  });
+});
 
 // start a server that listens on port 3000 and connects the sql client on success
-app.listen(3000, () => {
+app.listen(PORT, () => {
+  console.log(`App listening on http://localhost:${PORT}`);
   client.connect();
 });
